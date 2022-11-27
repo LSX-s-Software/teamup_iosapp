b02d8e474d35a789265949fedfcdd9edba86cb56
LSX-s-Software <2682191498@qq.com>
2022-11-27 23:19:36 +0800

    Initial Commit

---
+++ teamup_iosapp/TeamupApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamupApp.swift
+//  teamup_iosapp
+//
+//  Created by 林思行 on 2022/11/27.
+//
+
+import SwiftUI
+
+@main
+struct TeamupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
